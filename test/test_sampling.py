2c2c5b538f0590b867dcc7318520d823e8961da7
Atharva Aalok <atharvaaalok@gmail.com>
2025-10-20 19:33:25 -0700

    Update test script to visualize uniform point samples on closed manifold.

--- test/test_sampling.py
+++ test/test_sampling.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import torch
 
 from geodiff.utils import sample_T
+from geodiff.transforms import closed_transform_3d
 
 
 num_pts = 10000
@@ -20,5 +21,18 @@ plt.gca().set_aspect('equal', adjustable='box')
 plt.xlim([0, 1]); plt.ylim([0, 1])
 plt.xlabel('t'); plt.ylabel('s')
 plt.title('Farthest Point Sampling')
+plt.tight_layout()
+plt.show()
+
+
+# Use the closed transform to compute points on the initial closed manifold
+closed_manifold = closed_transform_3d(T)
+
+fig, ax = plt.subplots(subplot_kw = {'projection': '3d'})
+ax.scatter(closed_manifold[:, 0], closed_manifold[:, 1], closed_manifold[:, 2], s = 6,
+           alpha = 0.85, linewidths = 0)
+ax.set_xlim([-1, 1]); ax.set_ylim([-1, 1]); ax.set_zlim([-1, 1]);
+ax.set_box_aspect([1, 1, 1])
+ax.set_title('Farthest Point Sampling with Uniform Closed Transform')
 plt.tight_layout()
 plt.show()
